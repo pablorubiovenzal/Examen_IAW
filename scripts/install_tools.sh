ebf2b6addebd5f88cb15d38301bcff229baa2a1a
pablo <pablo@example.com>
2024-12-16 13:11:42 +0000

    hola

--- scripts/install_tools.sh
+++ scripts/install_tools.sh
@@ -1,3 +1,21 @@
 sudo apt update
 
-sudo apt-get install 
+apt-get install php -y
+
+apt-get install php-mysql -y
+
+apt-get install libapache2-mod-php -y
+
+apt-get install php-xml -y
+
+apt-get install php-mbstring -y
+
+apt-get install php-curl -y
+
+apt-get install php-zip -y
+
+apt-get install php-gd -y
+
+apt-get install php-intl -y
+
+apt-get install php-soap -y
